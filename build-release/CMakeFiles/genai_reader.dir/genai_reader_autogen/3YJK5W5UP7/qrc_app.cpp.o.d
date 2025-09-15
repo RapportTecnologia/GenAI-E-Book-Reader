8b3c05f4dbf3d8e0198ea45f0ce4387748eae538
Carlos Delfino <consultoria@carlosdelfino.eti.br>
2025-09-15 09:28:21 -0300

    Removido diretório de build que foi adicionado acidentalmente.

--- build-release/CMakeFiles/genai_reader.dir/genai_reader_autogen/3YJK5W5UP7/qrc_app.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/genai_reader.dir/genai_reader_autogen/3YJK5W5UP7/qrc_app.cpp.o: \
- /home/carlosdelfino/workspace/Rapport/Generativa/GenAi-E-Book-Reader/build-release/genai_reader_autogen/3YJK5W5UP7/qrc_app.cpp \
- /usr/include/stdc-predef.h
